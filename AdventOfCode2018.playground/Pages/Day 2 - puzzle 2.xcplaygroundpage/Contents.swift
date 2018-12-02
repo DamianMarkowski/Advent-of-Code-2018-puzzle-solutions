63400f7d63ade0977a763655586332d9f2064a46
Damian Markowski <contact@damianmarkowski.pl>
2018-12-02 12:34:25 +0000

    day 2 puzzle 2 - unit tests added

--- AdventOfCode2018.playground/Pages/Day 2 - puzzle 2.xcplaygroundpage/Contents.swift
+++ AdventOfCode2018.playground/Pages/Day 2 - puzzle 2.xcplaygroundpage/Contents.swift
@@ -1,3 +1,5 @@
+import XCTest
+
 func findCommonLettersBetweenTwoCorrectBoxIDs(dataFileName: String) -> String? {
     guard let fileContent = DataReader.readDataFromFile(dataFileName: dataFileName) else { return nil }
     let lines = DataReader.splitDataFromFileIntoLines(fileContent: fileContent)
@@ -8,9 +10,11 @@ func findCommonLettersBetweenTwoCorrectBoxIDs(dataFileName: String) -> String? {
                 let difference = zip(line, lineInternal).filter{ $0 != $1 }
                 if difference.count == 1 {
                     guard let differentLetterInWord1 = difference.first?.0,
-                        let differentLetterInWord2 = difference.first?.1 else { return nil }
-                    if line.firstIndex(of: differentLetterInWord1) == lineInternal.firstIndex(of: differentLetterInWord2) {
-                        commonLetters = line.replacingOccurrences(of: differentLetterInWord1.description, with: "")
+                        let differentLetterInWord2 = difference.first?.1, let indexInWord1 = line.firstIndex(of: differentLetterInWord1), let indexInWord2 = lineInternal.firstIndex(of: differentLetterInWord2) else { return nil }
+                    if indexInWord1 == indexInWord2 {
+                        var textToChange = line
+                        textToChange.remove(at: indexInWord1)
+                        commonLetters = textToChange
                         break
                     }
                 }
@@ -20,5 +24,18 @@ func findCommonLettersBetweenTwoCorrectBoxIDs(dataFileName: String) -> String? {
     return commonLetters
 }
 
-let commonLetters = findCommonLettersBetweenTwoCorrectBoxIDs(dataFileName: "day2-puzzle2-test-data")
+let commonLetters = findCommonLettersBetweenTwoCorrectBoxIDs(dataFileName: "day2-input-data")
 print(String(describing: commonLetters))
+
+class Puzzle2Tests: XCTestCase {
+    
+    func test_findCommonLettersBetweenTwoCorrectBoxIDs_shouldReturnCorrectValue_whenFirstTestDataPassed() {
+        XCTAssertEqual(findCommonLettersBetweenTwoCorrectBoxIDs(dataFileName: "day2-puzzle2-test-data"), "fgij")
+    }
+    
+    func test_findCommonLettersBetweenTwoCorrectBoxIDs_shouldReturnCorrectValue_whenMainTestDataPassed() {
+        XCTAssertEqual(findCommonLettersBetweenTwoCorrectBoxIDs(dataFileName: "day2-input-data"), "qysdtrkloagnfozuwujmhrbvx")
+    }
+}
+
+Puzzle2Tests.defaultTestSuite.run()
